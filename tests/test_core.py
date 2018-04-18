bfc85a8e2feb2b515fc73c5d9e4b33df6c2ec7ba
sumioka-sho <sumioka-sho@toei-anim.co.jp>
2018-04-18 16:18:39 +0900

    fix: ci

--- tests/test_core.py
+++ tests/test_core.py
@@ -8,11 +8,6 @@ class TestCore(unittest.TestSuite):
     def test_vector(self):
         assert (yr.YVector() == yr.YVector())
 
-    def test_app(self):
-        color = yr.YColor()
-        matrix = yr.YMatrix()
-        vec = yr.YVector()
-
 
 class TestPerformance(unittest.TestSuite):
     def test_coverage(self):
--- yurlungur/core/__init__.py
+++ yurlungur/core/__init__.py
@@ -2,5 +2,5 @@ from app import application
 from builder import Builder
 from command import file, cmd
 from proxy import YObject, YFile, YNode
-from enviroment import Maya, Houdini, Unreal
+from enviroment import installed, Maya, Houdini, Unreal
 
