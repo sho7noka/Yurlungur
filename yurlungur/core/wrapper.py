1feb6341d14f58794403ee349efcd8671e642aa6
sho7noka <alia_japan7@yahoo.co.jp>
2018-07-10 01:29:34 +0900

    update: wrap

--- yurlungur/core/wrapper.py
+++ yurlungur/core/wrapper.py
@@ -1,36 +1,12 @@
 # -*- coding: utf-8 -*-
+
+import ctypes
 import inspect
-import logging
 
 import yurlungur
 from yurlungur.core import app, env
 from yurlungur.tool import util
-from yurlungur.tool import meta
 
-
-class GuiLogHandler(logging.Handler):
-    def __init__(self):
-        super(GuiLogHandler, self).__init__()
-        from maya.OpenMaya import MGlobal
-        self.MGlobal = MGlobal
-
-    def emit(self, record):
-        msg = self.format(record)
-        if record.levelno > logging.WARNING:
-            # Error (40) Critical (50)
-            self.MGlobal.displayError(msg)
-            hou.ui.setStatusMessage(msg, severity=hou.severityType.Error)
-            bpy.ops.error.message('INVOKE_DEFAULT', type = "Error", message = msg)
-        elif record.levelno > logging.INFO:
-            # Warning (30)
-            self.MGlobal.displayWarning(msg)
-            hou.ui.setStatusMessage(msg, severity=hou.severityType.Warning)
-            bpy.ops.error.message('INVOKE_DEFAULT', type = "Error", message = msg)
-        else:
-            # Debug (10) and Info (20)
-            self.MGlobal.displayInfo(msg)
-            hou.ui.setStatusMessage(msg, severity=hou.severityType.Message)
-            bpy.ops.error.message('INVOKE_DEFAULT', type = "Error", message = msg)
 
 class YException(NotImplementedError):
     """
@@ -57,6 +33,35 @@ class YMObject(object):
     def module(self):
         """current application module"""
         return app.application.__name__
+
+
+class OpenGL(object):
+    """opelGL wrapper"""
+
+    def __getattr__(self, item):
+        def _getGL(mod):
+            for cmd, _ in inspect.getmembers(mod):
+                if cmd == item:
+                    setattr(
+                        self, cmd,
+                        (lambda str: dict(inspect.getmembers(mod))[str])(cmd)
+                    )
+                    return getattr(self, item)
+        _tmp = []
+
+        if env.Maya():
+            from maya import OpenMayaRender as _mgl
+            _tmp.extend(_mgl, _mgl.MHardwareRenderer.theRenderer().glFunctionTable())
+
+        if env.Blender():
+            import bgl
+            _tmp.extend(bgl)
+
+        for gl in _tmp:
+            if _getGL(gl):
+                return _getGL(gl)
+
+        return ctypes.cdll.OpenGL32
 
 
 class ORM(object):
@@ -86,6 +91,8 @@ _YObject = MetaObject("YObject", (object,), {"__doc__": MetaObject.__doc__})
 _YNode = MetaNode("YNode", (object,), {"__doc__": MetaNode.__doc__})
 _YAttr = MetaAttr("YAttr", (object,), {"__doc__": MetaAttr.__doc__})
 
+meta = YMObject()
+
 if env.Maya():
     import maya.api.OpenMaya as OM
 
@@ -94,7 +101,6 @@ if env.Maya():
     _YColor = type('_YColor', (OM.MColor,), dict())
 
 elif env.Houdini() or env.Unreal():
-    meta = YMObject()
 
     _YVector = type('_YVector', (
         meta.Vector if hasattr(meta, "Vector") else meta.Vector3,
@@ -105,3 +111,10 @@ elif env.Houdini() or env.Unreal():
     ), dict())
 
     _YColor = type('_YColor', (meta.Color,), dict())
+
+elif env.Blender():
+    import mathutils
+    
+    _YVector = type('_YVector', (mathutils.Vector,), dict())
+    _YMatrix = type('_YMatrix', (mathutils.Matrix,), dict())
+    _YColor = type('_YColor', (mathutils.Color,), dict())
