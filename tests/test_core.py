937384b16ecbb73112318c95a3a6f235aa7652e5
sho7noka <alia_japan7@yahoo.co.jp>
2018-07-10 00:36:34 +0900

    fix: tests rename

--- tests/test_core.py
+++ tests/test_core.py
@@ -2,18 +2,12 @@
 
 import doctest
 import unittest
-
-import yurlungur as yr
 from yurlungur.core import app
 
 
 class TestCore(unittest.TestSuite):
-
     def test_app(self):
         self.assertTrue(app.application == "standalone")
-
-    def test_vector(self):
-        self.assertTrue(yr.YVector() == yr.YVector())
 
 
 if __name__ == '__main__':
