7585074634c92e67aefa20037c3917bdfce4e8dd
sho7noka <alia_japan7@yahoo.co.jp>
2019-03-12 01:56:06 +0900

    add some adapter

--- yurlungur/adapters/ue.py
+++ yurlungur/adapters/ue.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 import unreal
 
 
@@ -18,4 +19,35 @@ class MaterialEditingLib(unreal.MaterialEditingLibrary):
 
 @unreal.uclass()
 class GetAnimationLibrary(unreal.AnimationLibrary):
+    pass
+
+
+class Project(object):
+    def __init__(self, project):
+        self.project = project
+
+    @property
+    def sequences(self):
+        return Timeline(self.project)
+
+
+class Timeline(object):
+    def __init__(self, timeline):
+        self.timeline = timeline
+
+    @property
+    def tracks(self):
+        return Track(self.timeline)
+
+
+class Track(object):
+    def __init__(self, track):
+        self.track = track
+
+    @property
+    def clips(self):
+        return Item(self.track)
+
+
+class Item(object):
     pass
