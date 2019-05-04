131accfc20e814b39e78a74a8183a009b23b13d0
sho7noka <alia_japan7@yahoo.co.jp>
2019-05-05 08:03:07 +0900

    update: app

--- pyembeded-win.ps1
+++ pyembeded-win.ps1
@@ -1,6 +1,26 @@
-Invoke-WebRequest "https://www.python.org/ftp/python/3.6.4/python-3.6.4-embed-amd64.zip" -O "epython_zip.zip"
-Expand-Archive -Path epython_zip.zip -DestinationPath epython
-cd epython
+# embeded python
+Invoke-WebRequest "https://www.python.org/ftp/python/3.6.4/python-3.6.4-embed-amd64.zip" -O "Python.zip"
+Expand-Archive -Path Python.zip -DestinationPath Python
+Remove-Item Python.zip
+
+# get pip
+Set-Location Python
 Invoke-WebRequest "https://bootstrap.pypa.io/get-pip.py" -OutFile "get-pip.py"
-python get-pip.py
-python -m pip install pyside2
+./python.exe get-pip.py
+Remove-Item get-pip.py
+
+# set path
+$pth = @"
+python36.zip
+.
+Lib/site-packages
+Scripts
+
+# Uncomment to run site.main() automatically
+#import site
+"@
+New-Item python36._pth -itemType File -Force -Value $pth
+
+#install module
+./python.exe -m pip install yurlungur
+
