e473e2de85c898e2108a4bb675feb0f6463e7531
sho7noka <alia_japan7@yahoo.co.jp>
2019-05-04 14:39:19 +0900

    fix: powershell

--- pyembeded-win.ps1
+++ pyembeded-win.ps1
@@ -1,4 +1,6 @@
-cd 
-wget "https://www.python.org/ftp/python/3.6.4/python-3.6.4-embed-amd64.zip" -O "epython_zip.zip"
+Invoke-WebRequest "https://www.python.org/ftp/python/3.6.4/python-3.6.4-embed-amd64.zip" -O "epython_zip.zip"
 Expand-Archive -Path epython_zip.zip -DestinationPath epython
-python -m pip install pyside
+cd epython
+Invoke-WebRequest "https://bootstrap.pypa.io/get-pip.py" -OutFile "get-pip.py"
+python get-pip.py
+python -m pip install pyside2
