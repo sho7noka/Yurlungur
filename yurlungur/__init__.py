8e68dda28f81e2df66dc20e183ea2983c244bf60
sho7noka <alia_japan7@yahoo.co.jp>
2019-10-26 22:25:30 +0900

    fix: init

--- yurlungur/__init__.py
+++ yurlungur/__init__.py
@@ -1,13 +1,15 @@
+# -*- coding: utf-8 -*-
 import sys
 
 assert sys.version_info > (2, 7), ('yurlungur currently requires Python 2.7 later')
 sys.dont_write_bytecode = True
 
-from yurlungur.core import *  # noQA
-from yurlungur.tool import meta, ui
-
 __all__ = []
 __version__ = "0.9.6"
+
+from yurlungur.core import *  # noQA
+from yurlungur.tool import meta, ui
+from yurlungur.tool.standalone import *  # noQA
 
 # info
 name = __name__
