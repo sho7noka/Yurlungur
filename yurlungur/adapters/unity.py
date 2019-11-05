aff06f6d0338d217f5c4b08da93640f1e472c19e
sho7noka <alia_japan7@yahoo.co.jp>
2019-11-05 23:57:29 +0900

    fix: typo

--- yurlungur/adapters/unity.py
+++ yurlungur/adapters/unity.py
@@ -1,7 +1,13 @@
 # coding: utf-8
 try:
+    from yurlungur.core.plugin import Plugbase
     import UnityEngine
 except ImportError:
     from yurlungur.core.env import App as _
 
     run, shell, end = _("unity")._actions
+
+
+class Unity(Plugbase):
+    def proxy(self):
+        pass
