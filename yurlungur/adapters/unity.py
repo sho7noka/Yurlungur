f0006dad2a8ed80ef3e1f2c75699662ab91ae7f2
sho7noka <alia_japan7@yahoo.co.jp>
2020-05-13 14:04:37 +0900

    fix: unity

--- yurlungur/adapters/unity.py
+++ yurlungur/adapters/unity.py
@@ -1,22 +1,68 @@
 # coding: utf-8
-
-# https://docs.unity3d.com/Packages/com.unity.scripting.python@2.0/manual/index.html
 try:
+    import clr
     import UnityEngine
     import UnityEditor
 
     getattr(UnityEngine, "Debug")
 
 except (ImportError, AttributeError):
+    import textwrap
     from yurlungur.core.env import App as _
 
     run, shell, end = _("unity")._actions
 
 
+    def initialize_package(path):
+        """
+        https://docs.unity3d.com/Packages/com.unity.package-manager-ui@2.0/manual/index.html
+
+        Args:
+            path:
+
+        Returns:
+
+        """
+        cs_text = textwrap.dedent("""
+        using UnityEngine;
+        using System.IO;
+        using System.Reflection;
+        using System.Collections;
+    
+        #if UNITY_EDITOR
+        using UnityEditor;
+        using UnityEditor.SceneManagement;
+    
+        [InitializeOnLoad]
+        public class Startup {
+    
+            static void Startup()
+            {
+                UnityEditor.PackageManager.Client.Add("com.unity.scripting.python");
+            }
+    
+            [MenuItem("Examples/Execute menu items")]
+            static void EditorPlaying()
+            {
+                var newScene = EditorSceneManager.NewScene(NewSceneSetup.EmptyScene, NewSceneMode.Single);
+                EditorApplication.ExecuteMenuItem("GameObject/3D Object/Cube");
+                EditorSceneManager.SaveScene(newScene, "Assets/MyNewScene.unity");
+                EditorApplication.Exit(0);
+            }
+        }
+        #endif
+        """)
+
+        with file(path) as f:
+            f.write(cs_text)
+
+
 def Console():
-    typeName = "UnityEngine.MonoBehaviour"
-    # print System.Reflection.Assembly.Load("UnityEngine.dll").GetType(typeName);
-    import clr
+    """
+
+    Returns:
+
+    """
     import System
     for asm in System.AppDomain.CurrentDomain.GetAssemblies():
         print asm.ToString()
@@ -24,50 +70,18 @@ def Console():
 
 
 def EvalScript(script):
-    import clr
-    import System.IO
+    """
 
-    # eval C# from Roslyn
+    Args:
+        script:
+
+    Returns:
+
+    """
+    import System.IO
     try:
         clr.AddReference("Microsoft.CodeAnalysis.CSharp.Scripting")
-        Microsoft.CodeAnalysis.CSharp.Scripting.CSharpScript.EvaluateAsync(script)
+        import Microsoft.CodeAnalysis.CSharp.Scripting as scripting
+        scripting.CSharpScript.EvaluateAsync(script)
     except System.IO.FileNotFoundException:
         pass
-
-
-"""
-using UnityEngine;
-using System.IO;
-using System.Reflection;
-using System.Collections;
-
-#if UNITY_EDITOR
-using System;
-using UnityEditor;
-using UnityEditor.SceneManagement;
-
-[InitializeOnLoad]
-public class Startup {
-    static Startup()
-    {
-        UnityEditor.PackageManager.Client.Add("com.unity.scripting.python");
-    }
-}
-
-[UnityEditor.InitializeOnLoad]
-public class ExampleClass
-{
-    [MenuItem("Examples/Execute menu items")]
-    static void EditorPlaying()
-    {
-        // https://docs.unity3d.com/Packages/com.unity.package-manager-ui@2.0/manual/index.html
-        UnityEditor.PackageManager.Client.Add("com.unity.scripting.python");
-
-        var newScene = EditorSceneManager.NewScene(NewSceneSetup.EmptyScene, NewSceneMode.Single);
-        EditorApplication.ExecuteMenuItem("GameObject/3D Object/Cube");
-        EditorSceneManager.SaveScene(newScene, "Assets/MyNewScene.unity");
-        EditorApplication.Exit(0);
-    }
-}
-#endif
-"""
